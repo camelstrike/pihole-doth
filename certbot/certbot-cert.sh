97455de08f929f224b525e659cd6a1bd356354a0
camelstrike <gmdibiase@gmail.com>
2022-02-26 15:12:17 +0000

    Added script to generate ssl certs and renew them

---
+++ certbot/certbot-cert.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+CERTBOT_DIR="/etc/letsencrypt/live"
+CERTBOT_RELOAD_NGINX="ssh -q -o StrictHostKeyChecking=no -i /opt/pihole-doth/id_ed25519 nginx nginx -s reload"
+
+CERTBOT_ARGS=$(
+if [ "$CERTBOT_ENV" = "staging" ]
+ then
+  echo "$CERTBOT_DOMAIN --test-cert"
+elif [ "$CERTBOT_ENV" = "production" ]
+ then
+  echo "$CERTBOT_DOMAIN"
+else
+ echo "error"
+fi
+)
+IS_CERT_AVAIL=$([ -d "$CERTBOT_DIR"/"$CERTBOT_DOMAIN" ] && echo true || echo false)
+IS_CERT_SELF_SIGNED=$([ "$(openssl x509 -in "$CERTBOT_DIR"/"$CERTBOT_DOMAIN"/fullchain.pem -text -noout | grep -oE 'Subject: CN =.*' | cut -d" " -f4)" = "localhost" ] && echo true || echo false)
+
+if [ "$CERTBOT_ARGS" != "error" ] && [ "$IS_CERT_SELF_SIGNED" = "true" ]
+ then
+  rm -r "${CERTBOT_DIR:?}/"*
+  certbot certonly \
+  --dns-cloudflare \
+  --no-eff-email \
+  --agree-tos \
+  --preferred-chain "ISRG Root X1" \
+  -m "$CERTBOT_EMAIL" \
+  --dns-cloudflare-credentials /opt/pihole-doth/cloudflare.ini \
+  --dns-cloudflare-propagation-seconds 30 \
+  -d $CERTBOT_ARGS \
+  --post-hook "$CERTBOT_RELOAD_NGINX"
+elif [ "$IS_CERT_AVAIL" = "true" ] && [ "$CERTBOT_ARGS" != "error" ] && [ "$IS_CERT_SELF_SIGNED" = "false" ]
+ then
+    certbot renew --post-hook "$CERTBOT_RELOAD_NGINX"
+else
+ echo "Something is off or cert is self-signed, check logs"
+ exit 0
+fi
