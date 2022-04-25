7b5680e64bca27e78a7139c8b9c21e685d4c084c
camelstrike <gmdibiase@gmail.com>
2022-04-25 15:26:43 +0000

    Make nginx create self signed certs to prevent race condition with certbot

--- certbot/start-services.sh
+++ certbot/start-services.sh
@@ -3,10 +3,6 @@
 # Set cloudflare token
 echo "dns_cloudflare_api_token = $CLOUDFLARE_TOKEN" > /opt/pihole-doth/cloudflare.ini
 
-# Create letsencrypt dir and generate self-signed certs
-mkdir -p /etc/letsencrypt/live/${CERTBOT_DOMAIN}
-openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout /etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem -out /etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem -subj '/CN=localhost'
-
 # Generate letsencrypt cert
 /opt/pihole-doth/certbot-cert.sh
 
--- nginx/start-services.sh
+++ nginx/start-services.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Create letsencrypt dir and generate self-signed certs
+mkdir -p /etc/letsencrypt/live/${CERTBOT_DOMAIN}
+openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout /etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem -out /etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem -subj '/CN=localhost'
+
 # Generate template for our domain
 envsubst '${CERTBOT_DOMAIN}' < /tmp/nginx.conf.template > /etc/nginx/nginx.conf
 
