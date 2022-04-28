90100f8613fd7e7f8657a0f36e5df868db6659ec
camelstrike <gmdibiase@gmail.com>
2022-04-28 13:52:07 +0000

    - Added pihole-updatelists ENV VARs config - Added tini to supervise processes and forward signals - Added circleci build image config

--- build/nginx/start-services.sh
+++ build/nginx/start-services.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
 # Create letsencrypt dir and generate self-signed certs
-mkdir -p /etc/letsencrypt/live/${CERTBOT_DOMAIN}
-openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout /etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem -out /etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem -subj '/CN=localhost'
+if [ ! -d /etc/letsencrypt/live/${CERTBOT_DOMAIN} ]
+then
+    mkdir -p /etc/letsencrypt/live/${CERTBOT_DOMAIN}
+    openssl req -x509 -nodes -newkey rsa:4096 -days 1 -keyout /etc/letsencrypt/live/${CERTBOT_DOMAIN}/privkey.pem -out /etc/letsencrypt/live/${CERTBOT_DOMAIN}/fullchain.pem -subj '/CN=localhost'
+else
+    continue
+fi
 
 # Generate template for our domain
 envsubst '${CERTBOT_DOMAIN}' < /tmp/nginx.conf.template > /etc/nginx/nginx.conf
